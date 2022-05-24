905deb098470e141f6f7e48c5ddea971db43bb03
HenriqueBaesa <62488513+HenriqueBaesa@users.noreply.github.com>
2022-05-24 14:26:27 -0300

    return computer objetc

--- Repositories/ComputerRepository.cs
+++ Repositories/ComputerRepository.cs
@@ -43,7 +43,7 @@ class ComputerRepository
         return computers;
     }
 
-    public void Save(Computer computer)
+    public Computer Save(Computer computer)
     {
         var conn = new SqliteConnection(databaseConfig.ConnectionString);
         conn.Open();
@@ -53,8 +53,10 @@ class ComputerRepository
         command.Parameters.AddWithValue("$id", computer.Id);
         command.Parameters.AddWithValue("$ram", computer.Ram);
         command.Parameters.AddWithValue("$processor", computer.Processor);
-        command.ExecuteNonQuery();
 
+        command.ExecuteNonQuery();
         conn.Close();
+
+        return computer;
     }
 }
