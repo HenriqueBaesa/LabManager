44422fabbe1f38432509f49723b003137aa9a454
HenriqueBaesa <62488513+HenriqueBaesa@users.noreply.github.com>
2022-05-24 14:18:40 -0300

    Setup de banco de dados, classe Computer, acesso a dados para Computer
    Cria a classe de setup de banco de dados, implementa a classe Computer, implementa a classe de acesso a dados para Computer

--- Program.cs
+++ Program.cs
@@ -1,25 +1,13 @@
 ﻿
+using LabManager.Database;
+using LabManager.Models;
+using LabManager.Repositories;
 using Microsoft.Data.Sqlite;
 
-var conn = new SqliteConnection("Data Source=database.db");
-conn.Open();
+var databaseConfig = new DatabaseConfig();
+var databaseSetup = new DatabaseSetup(databaseConfig);
 
-var command = conn.CreateCommand();
-command.CommandText = @"
-    CREATE TABLE IF NOT EXISTS Computers(
-        id int not null primary key,
-        ram varchar(100) not null,
-        processor varchar(100) not null
-    );
-    CREATE TABLE IF NOT EXISTS Lab(
-        id int not null primary key,
-        number varchar(100) not null,
-        block varchar(100) not null
-    );
-";
-command.ExecuteNonQuery();
-
-conn.Close();
+var computerRepository = new ComputerRepository(databaseConfig);
 
 // Routing
 var modelName = args[0];
@@ -29,42 +17,24 @@ if (modelName == "Computer")
 {
     if (modelAction == "List")
     {
-        conn = new SqliteConnection("Data Source=database.db");
-        conn.Open();
-
-        command = conn.CreateCommand();
-        command.CommandText = "SELECT * FROM Computers";
-
-        var reader = command.ExecuteReader();
 
         Console.WriteLine("Computer List");
-        while(reader.Read())
+        foreach (var computer in computerRepository.GetAll())
         {
-            Console.WriteLine("{0}, {1}, {2}", reader.GetInt32(0), reader.GetString(1), reader.GetString(2));
-            reader.GetInt32(0);
+            Console.WriteLine("{0}, {1}, {2}", computer.Id, computer.Ram, computer.Processor);
         }
-
-        reader.Close();
-        conn.Close();
     }
 
     if (modelAction == "New")
     {
-        conn = new SqliteConnection("Data Source=database.db");
-        conn.Open();
-
         int id = Convert.ToInt32(args[2]);
         string ram = args[3];
         string processor = args[4];
 
-        command = conn.CreateCommand();
-        command.CommandText = "INSERT INTO Computers VALUES($id, $ram, $processor)";
-        command.Parameters.AddWithValue("$id", id);
-        command.Parameters.AddWithValue("$ram", ram);
-        command.Parameters.AddWithValue("$processor", processor);
-        command.ExecuteNonQuery();
+        var computer = new Computer(id, ram, processor);
+        computerRepository.Save(computer);
 
-        conn.Close();
+        Console.WriteLine("Computer Created");
     }
 }
 
@@ -72,10 +42,10 @@ else if (modelName == "Lab")
 {
     if (modelAction == "List")
     {
-        conn = new SqliteConnection("Data Source=database.db");
+        var conn = new SqliteConnection("Data Source=database.db");
         conn.Open();
 
-        command = conn.CreateCommand();
+        var command = conn.CreateCommand();
         command.CommandText = "SELECT * FROM Lab";
 
         var reader = command.ExecuteReader();
@@ -93,14 +63,14 @@ else if (modelName == "Lab")
 
     if (modelAction == "New")
     {
-        conn = new SqliteConnection("Data Source=database.db");
+        var conn = new SqliteConnection("Data Source=database.db");
         conn.Open();
 
         int id = Convert.ToInt32(args[2]);
         string number = args[3];
         string block = args[4];
 
-        command = conn.CreateCommand();
+        var command = conn.CreateCommand();
         command.CommandText = "INSERT INTO Lab VALUES($id, $number, $block)";
         command.Parameters.AddWithValue("$id", id);
         command.Parameters.AddWithValue("$number", number);
---
+++ Repositories/ComputerRepository.cs
@@ -0,0 +1,60 @@
+
+using LabManager.Database;
+using LabManager.Models;
+using Microsoft.Data.Sqlite;
+
+namespace LabManager.Repositories;
+
+class ComputerRepository
+{
+    private DatabaseConfig databaseConfig;
+
+    public ComputerRepository(DatabaseConfig databaseConfig)
+    {
+        this.databaseConfig = databaseConfig;
+    }
+
+    public List<Computer> GetAll()
+    {
+        var conn = new SqliteConnection(databaseConfig.ConnectionString);
+        conn.Open();
+
+        var command = conn.CreateCommand();
+        command.CommandText = "SELECT * FROM Computers";
+
+        var reader = command.ExecuteReader();
+
+        var computers = new List<Computer>();
+
+        while(reader.Read())
+        {
+            computers.Add(
+                new Computer(
+                    reader.GetInt32(0),
+                    reader.GetString(1),
+                    reader.GetString(2)
+                )
+            );
+
+            reader.GetInt32(0);
+        }
+        conn.Close();
+
+        return computers;
+    }
+
+    public void Save(Computer computer)
+    {
+        var conn = new SqliteConnection(databaseConfig.ConnectionString);
+        conn.Open();
+
+        var command = conn.CreateCommand();
+        command.CommandText = "INSERT INTO Computers VALUES($id, $ram, $processor)";
+        command.Parameters.AddWithValue("$id", computer.Id);
+        command.Parameters.AddWithValue("$ram", computer.Ram);
+        command.Parameters.AddWithValue("$processor", computer.Processor);
+        command.ExecuteNonQuery();
+
+        conn.Close();
+    }
+}
